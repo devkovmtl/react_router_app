bc31f70927a195f29b6e1bfa53fbb2ab52bbf741
Mikael <dev.kov.mtl@gmail.com>
2022-05-21 13:07:53 -0400

    Nested Routes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import { Home, About, Products, Error } from './pages';
 function App() {
   return (
     <BrowserRouter>
+      <nav>Our Nav</nav>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='about' element={<About />} />
-        <Route path='products' element={<Products />} />
-        <Route path='*' element={<Error />} />
+        <Route path='/' element={<Home />}>
+          <Route path='about' element={<About />} />
+          <Route path='products' element={<Products />} />
+          <Route path='*' element={<Error />} />
+        </Route>
+        <Route path='dashboard' element={<div>Dashboard</div>}>
+          <Route path='stats' element={<div>Stats</div>} />
+        </Route>
       </Routes>
+      <footer>Our Footer</footer>
     </BrowserRouter>
   );
 }
