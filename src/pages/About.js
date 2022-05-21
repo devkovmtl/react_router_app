97b46553b2b1b9fee8aec5867c17f2587a5731d1
Mikael <dev.kov.mtl@gmail.com>
2022-05-21 12:28:10 -0400

    Link Component

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const About = () => {
   return (
     <section className='section'>
       <h2>About</h2>
+      <Link to='/about' className='btn'>
+        About
+      </Link>
     </section>
   );
 };
